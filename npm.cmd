1225822035f6739233a2a72e8c48716e91904ec4
Vaishnavi Pandey <vaishnavipandey0210@gmail.com>
2026-04-30 22:42:40 +0530

    Delete npm.cmd

--- npm.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set NODE_DIR=%~dp0\node\node-v24.14.1-win-x64
-if not exist "%NODE_DIR%\node.exe" (
-  echo Portable Node.js not found in %NODE_DIR%.
-  exit /b 1
-)
-set PATH=%NODE_DIR%;%PATH%
-"%NODE_DIR%\npm.cmd" %*
